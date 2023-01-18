bb8eb1cd9c6918ccfe1c4d5538ff44d86ba1d92c
Nikita Balashov <nikita.89534588306@gmail.com>
2023-01-18 16:48:53 +0500

    доработана структура папок. добален метод для парсинга URL

---
+++ app/controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+	class Controller{
+		public function index(){
+			echo "home/index";
+		}
+		
+		public function about(){
+			echo "home/about"; 
+		}  
+	}
--- app/core/app.php
+++ app/core/app.php
@@ -1,6 +1,19 @@
 <?php
 	class App {
+
+		protected $controller = "home";
+		protected $method = "index";
+		protected $params = [];
+		
 		public function __construct(){
-			echo 'Hello';
-		} 
+			$url = $this->parseUrl();
+		}
+
+		private function parseUrl(){
+			if(isset($_GET['url'])){
+				return explode('/', filter_var(
+					rtrim($_GET['url'], '/'),
+					FILTER_SANITIZE_STRING));
+			}
+		}
 	}
